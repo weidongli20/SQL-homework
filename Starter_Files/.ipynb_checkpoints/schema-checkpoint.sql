7c86a779db7b6d829ce81820847aa125c94dc7ce
W L <weidong_ca00@yahoo.com>
2020-07-24 18:00:44 -0500

    update files

--- Starter_Files/.ipynb_checkpoints/schema-checkpoint.sql
+++ Starter_Files/.ipynb_checkpoints/schema-checkpoint.sql
@@ -1,7 +1,3 @@
--- Drop Views
-DROP VIEW IF EXISTS anomalous_payments;
-DROP VIEW IF EXISTS small_payments;
-
 -- Drop table if exists
 DROP TABLE IF EXISTS card_holder;
 DROP TABLE IF EXISTS credit_card;
@@ -12,30 +8,24 @@ DROP TABLE IF EXISTS transactions;
 -- Create new table
 CREATE TABLE card_holder (
   card_holder_id INT NOT NULL,
-  card_holder_name VARCHAR(50) NOT NULL,
-  PRIMARY KEY (card_holder_id)
+  card_holder_name VARCHAR(50) NOT NULL
 );
 
 -- Create new table to import data
 CREATE TABLE credit_card (
   card VARCHAR(20) NOT NULL,
-  card_holder_id INT NOT NULL,
-  PRIMARY KEY (card),
-  FOREIGN KEY (card_holder_id) REFERENCES card_holder(card_holder_id)
+  card_holder_id INT NOT NULL
 );
 
 CREATE TABLE merchant_category (
   merchant_cat_id INT NOT NULL,
-  merchant_cat_name VARCHAR(50) NOT NULL,
-  PRIMARY KEY (merchant_cat_id)
+  merchant_cat_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE merchant (
   merchant_id INT NOT NULL,
   merchant_name VARCHAR(50) NOT NULL,
-  merchant_cat_id INT NOT NULL,
-  PRIMARY KEY (merchant_id),
-  FOREIGN KEY (merchant_cat_id) REFERENCES merchant_category(merchant_cat_id)
+  merchant_cat_id INT NOT NULL
 );
 
 CREATE TABLE transactions (
@@ -43,8 +33,5 @@ CREATE TABLE transactions (
   trans_date timestamp without time zone NOT NULL,
   amount FLOAT,
   card VARCHAR(20) NOT NULL,
-  merchant_id INT NOT NULL,
-  PRIMARY KEY (transactions_id),
-  FOREIGN KEY (merchant_id) REFERENCES merchant(merchant_id),
-  FOREIGN KEY (card) REFERENCES credit_card(card)
+  merchant_id INT NOT NULL
 );
--- Starter_Files/query.sql
+++ Starter_Files/query.sql
@@ -1,47 +1,7 @@
-SELECT * FROM transactions;
-SELECT * FROM card_holder;
-SELECT * FROM credit_card;
+-- For the time period 7:00 am to 9:00 am
+-- The 100 highest transactions during
 
-SELECT *
-FROM transactions t
-JOIN credit_card c ON t.card = c.card
-JOIN card_holder a ON a.card_holder_id = c.card_holder_id
-;
-
-SELECT trans_date, card, COUNT(*)
-FROM transactions
-GROUP BY card, trans_date
-ORDER BY trans_date
-LIMIT 5
-;
-
-SELECT *
-FROM card_holder
-;
-
-SELECT 
-  c.card_holder_name, 
-  COUNT(t.trans_date) "Total Transactions",
-  MIN(t.amount) "Minimum Amount"
-FROM card_holder c
-JOIN credit_card r ON c.card_holder_id = r.card_holder_id
-JOIN transactions t ON t.card = r.card
-GROUP BY c.card_holder_name
-ORDER BY c.card_holder_name
-;
-
-
-SELECT 
-  c.card_holder_name, 
-  t.amount "Minimum Amount"
-FROM card_holder c
-JOIN credit_card r ON c.card_holder_id = r.card_holder_id
-JOIN transactions t ON t.card = r.card
-GROUP BY c.card_holder_name, t.amount
-ORDER BY c.card_holder_name, t.amount DESC
-;
-
-
+CREATE VIEW top_100_trans_7_9 AS
 SELECT 
   trans_date,
   amount,
@@ -54,129 +14,28 @@ ORDER BY amount DESC
 LIMIT 100
 ;
 
---ALTER TABLE transactions RENAME merchat_id TO merchant_id
---;
-
+-- Small payments
+CREATE VIEW small_payments AS
 SELECT
   m.merchant_name,
-  COUNT(t.amount) "Small Trans Numbers"
+  COUNT(t.amount) "Small Trans Numbers",
+  MIN(t.amount) "Least Payment"
 FROM transactions t
 JOIN merchant m ON t.merchant_id = m.merchant_id
 WHERE t.amount < 2
 GROUP BY m.merchant_name
-ORDER BY COUNT(t.amount) DESC
-LIMIT 5
+ORDER BY MIN(t.amount)
 ;
-
-
-SELECT 
-  c.card_holder_id, 
-  COUNT(t.trans_date) "Total Transactions",
-  MIN(t.amount) "Minimum Amount"
-FROM card_holder c
-JOIN credit_card r ON c.card_holder_id = r.card_holder_id
-JOIN transactions t ON t.card = r.card
-WHERE (c.card_holder_id = 2 OR c.card_holder_id = 18)
-GROUP BY c.card_holder_id
-;
-
-CREATE VIEW anomalous_payments AS
-SELECT 
-  c.card_holder_id,
-  c.card_holder_name,
-  COUNT(t.trans_date) "Total Transactions",
-  MIN(t.amount) "Minimum Amount"
-FROM card_holder c
-JOIN credit_card r ON c.card_holder_id = r.card_holder_id
-JOIN transactions t ON t.card = r.card
-WHERE (t.amount*100-ROUND(t.amount*100))>0
-GROUP BY c.card_holder_id, c.card_holder_name
-ORDER BY c.card_holder_id
-;
-
-DROP VIEW IF EXISTS small_payments;
-
-CREATE VIEW small_payments AS
-SELECT 
-  c.card_holder_id,
-  c.card_holder_name,
-  COUNT(t.trans_date) "trans_count",
-  MIN(t.amount) "min_amount"
-FROM card_holder c
-JOIN credit_card r ON c.card_holder_id = r.card_holder_id
-JOIN transactions t ON t.card = r.card
+-- top 5
+CREATE VIEW small_payments_top5_merchants AS
+SELECT
+  m.merchant_name,
+  COUNT(t.amount) "Small Trans Numbers",
+  MIN(t.amount) "Least Payment"
+FROM transactions t
+JOIN merchant m ON t.merchant_id = m.merchant_id
 WHERE t.amount < 2
-GROUP BY c.card_holder_id, c.card_holder_name
-ORDER BY c.card_holder_id
-;
-
-SELECT trans_count, min_amount
-FROM small_payments
-;
-
-SELECT 
-  s.card_holder_id,
-  s.card_holder_name,
-  trans_count,
-  min_amount
-FROM small_payments s
-JOIN anomalous_payments a
-ON s.card_holder_id = a.card_holder_id
-ORDER BY trans_count DESC
-;
-
--- visualization
-
-SELECT *
-FROM card_holder
-WHERE card_holder_id = 2 OR card_holder_id = 18
-;
-
-SELECT 
-  c.card_holder_id, 
-  c.card_holder_name,
-  COUNT(t.trans_date) "Total Transactions",
-  MIN(t.amount) "Minimum Amount"
-FROM card_holder c
-JOIN credit_card r ON c.card_holder_id = r.card_holder_id
-JOIN transactions t ON t.card = r.card
-WHERE (c.card_holder_id = 2 OR c.card_holder_id = 18)
-GROUP BY c.card_holder_id, c.card_holder_name
-;
-
-SELECT 
-  *
-FROM card_holder c
-JOIN credit_card r ON c.card_holder_id = r.card_holder_id
-JOIN transactions t ON t.card = r.card
-WHERE (c.card_holder_id = 2 OR c.card_holder_id = 18)
-ORDER BY c.card_holder_id
-;
-
-
-SELECT 
-  trans_date,
-  amount,
-  card
-FROM transactions
-WHERE (
-	EXTRACT(HOUR FROM trans_date) > 7 
-    AND EXTRACT(HOUR FROM trans_date) < 9)
-ORDER BY amount DESC
-LIMIT 100
-;
-
-SELECT 
-  trans_date,
-  amount,
-  card
-FROM transactions
-WHERE (
-	trans_date >= '2018-01-01'
-AND trans_date < '2018-07-01'
-);
-
-SELECT *
-FROM transactions
-WHERE transactions_id = 3494
+GROUP BY m.merchant_name
+ORDER BY MIN(t.amount)
+LIMIT 5
 ;
